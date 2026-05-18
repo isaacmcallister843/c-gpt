17459bb7267aa528ee80c85a0d341bff9d61fa41
Isaac <isaac.mcallister@gmail.com>
2026-05-18 18:30:21 -0400

    bash update

--- cloud_instance_setup.sh
+++ cloud_instance_setup.sh
@@ -3,14 +3,15 @@ set -e
 
 echo "Installing system packages..."
 sudo apt-get update
-sudo apt-get install -y stockfish
+sudo apt-get install -y stockfish pip python3.11 python3.11-venv
+
+echo "Setting up venv..."
+python3.11 -m venv venv
+source venv/bin/activate
 
 echo "Installing Python dependencies..."
+pip install -U pip
 pip install -e .
 pip install -e ".[cloud]"
-
-# Renting cloud instances for DL generally come up with a pytorch + CUDA image, no need to resinstall CUDA
-# echo "Installing CUDA" 
-# pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
 
 echo "Done."
